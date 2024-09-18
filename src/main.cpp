1fef7a40ab6a1166eb8aef372967cba00bcfcf8f
Shenal Herath <smh231@uclive.ac.nz>
2024-09-18 17:42:16 +1200

    testing encoder reading (not finished)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
+#include "encoder.h"
 #include "motor.h"
 #include <Arduino.h>
 
 void setup()
 {
-    InitDriveMotors();
+    // InitDriveMotors();
+
+    InitDriveEncoders();
+
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    if ( (lastReportedPos1 != encoderPos1) || (lastReportedPos2 != encoderPos2) ) {
+        Serial.print("Index:");
+        Serial.print(encoderPos1, DEC);
+        Serial.print(":");
+        Serial.print(encoderPos2, DEC);
+        Serial.println();
+        lastReportedPos1 = encoderPos1;
+        lastReportedPos2 = encoderPos2;
+    }
 }
