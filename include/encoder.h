39440ce0505287641b8e04ca622904ce5b4027ad
Shenal Herath <smh231@uclive.ac.nz>
2024-09-25 22:32:39 +1200

    collection motor encoder working

--- include/encoder.h
+++ include/encoder.h
@@ -6,10 +6,15 @@
 #include <stdint.h>
 
 enum DriveEncoderPinAssignments {
-    leftMotorPinA = 2,
-    leftMotorPinB = 3,
-    rightMotorPinA = 4,
-    rightMotorPinB = 5,
+    leftEncoderPinA = 2,
+    leftEncoderPinB = 3,
+    rightEncoderPinA = 4,
+    rightEncoderPinB = 5,
+};
+
+enum CollectionMotorPinAssignments {
+    collectionEncoderPinA = 32,
+    collectionEncoderPinB = 33,
 };
 
 extern volatile unsigned int leftMotorPos;
@@ -17,13 +22,21 @@ extern unsigned int prevLeftMotorPos;
 extern volatile unsigned int RightMotorPos;
 extern unsigned int prevRightMotorPos;
 
+extern volatile unsigned int collectionMotorPos;
+extern unsigned int prevCollectionMotorPos;
+
 extern boolean leftASet;
 extern boolean leftBSet;
 extern boolean rightASet;
 extern boolean rightBSet;
 
+extern boolean collectionASet;
+extern boolean collectionBSet;
+
 void LeftEncoderIntHandler();
 void RightEncoderIntHandler();
+void CollectionEncoderIntHandler();
 void InitDriveEncoders();
+void InitCollectionEncoder();
 
 #endif
