987076df2635f116000755a38a344b7bcdd7dd2a
Shenal Herath <smh231@uclive.ac.nz>
2024-09-29 15:20:02 +1300

    IMU module implemented

--- include/imu.h
+++ include/imu.h
@@ -1,9 +1,18 @@
 #ifndef IMU_H
 #define IMU_H
 
-#include <stdint.h>
+#include <Adafruit_BNO055.h>
+#include <Adafruit_Sensor.h>
 
 #define IMU_SAMPLE_RATE_MS 100
+#define IMU_ID             55
+#define IMU_ADDRESS        0x28
 
+extern sensors_event_t orientationData, angVelocityData, linearAccelData, magnetometerData, accelerometerData, gravityData;
+extern Adafruit_BNO055 bno;
+extern int8_t boardTemp;
+
+void InitIMU();
+void UpdateIMU();
 
 #endif
