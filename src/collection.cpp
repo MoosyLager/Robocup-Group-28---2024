c5e4e180a1ad4e0abd1b61e017a61124835f66c8
Shenal Herath <smh231@uclive.ac.nz>
2024-10-02 21:15:35 +1300

    added collector calibrating code (untested)

--- src/collection.cpp
+++ src/collection.cpp
@@ -2,6 +2,17 @@
 
 void CalibrateCollector()
 {
+    Serial.println("Calibrating Collection...");
+    bool isCalibrated = false;
+    while ( !isCalibrated ) {
+        SetMotorSpeed(collectionMotor, MAX_MOTOR_VAL);
+        if ( !CollectorPosition() ) {
+            SetMotorSpeed(collectionMotor, MOTOR_STOP_VAL);
+            collectionMotorPos = 0;
+            prevCollectionMotorPos = 1;
+            isCalibrated = true;
+        }
+    }
 }
 
 void ActuateCollector()
