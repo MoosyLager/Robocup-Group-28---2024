46a73caaec0db8cb5a57deb1b293fde38b216830
Shenal Herath <smh231@uclive.ac.nz>
2024-10-02 20:14:58 +1300

    created collector module

---
+++ include/collection.h
@@ -0,0 +1,12 @@
+#ifndef COLLECTION_H
+#define COLLECTION_H
+
+#include "encoder.h"
+#include "motor.h"
+#include "sensor.h"
+
+void CalibrateCollector();
+void ActuateCollector();
+void ResetCollector();
+
+#endif
---
+++ src/collection.cpp
@@ -0,0 +1,13 @@
+#include "collection.h"
+
+void CalibrateCollector()
+{
+}
+
+void ActuateCollector()
+{
+}
+
+void ResetCollector()
+{
+}
