31f667065eaab40f60de88ac81d8af33440b84c7
Shenal Herath <smh231@uclive.ac.nz>
2024-10-03 22:05:56 +1300

    emptied main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,9 @@
-#include "collection.h"
-#include "encoder.h"
-#include "motor.h"
-#include "sensor.h"
 #include <Arduino.h>
 
 void setup()
 {
-    Serial.begin(9600);
-    InitCollectionEncoder();
-    InitCollectionMotor();
-    InitIOExpander();
-    InitLimitSwitch();
-    CalibrateCollector();
 }
 
 void loop()
 {
-    if ( prevCollectionMotorPos != collectionMotorPos ) {
-        Serial.println(collectionMotorPos);
-        prevCollectionMotorPos = collectionMotorPos;
-    }
 }
