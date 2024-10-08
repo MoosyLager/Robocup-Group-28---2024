a3dfe5b777e0f80c85d1defb5577438635355ab6
Shenal Herath <smh231@uclive.ac.nz>
2024-10-09 04:13:52 +1300

    changed such that blue button must be pressed for PID control to begin

--- src/main.cpp
+++ src/main.cpp
@@ -6,95 +6,47 @@
 #include <Arduino.h>
 #include <elapsedMillis.h>
 
-// elapsedMillis timer;
+RobotFSM fsm;
+elapsedMillis updateSensorsTimer, updateFSMTimer, updatePWMTimer, buttonTimer;
+bool pwmOn = false;
 
 void setup()
 {
     Serial.begin(9600);
-    // InitSensors();
-    // InitMotors();
-    // InitEncoders();
-    // initializeRobotFSM(&fsm);
-    // delay(7000);
-    // Serial.println("Setup complete");
-    // for ( int i = 0; i < 100; i++ ) {
-    //     UpdateTOFL0();
-    //     UpdateTOFL1();
+    InitSensors();
+    InitMotors();
+    InitEncoders();
+    initializeRobotFSM(&fsm);
+
+    for ( int i = 0; i < 2 * CIRCULAR_BUF_SIZE; i++ ) {
+        UpdateTOFL0();
+        UpdateTOFL1();
+    }
+
+    Serial.println("Setup complete");
 }
 
 void loop()
 {
-    // Serial.print("Centre Left: ");
-    // Serial.print(detectedCentreLeft());
-    // Serial.print(" Centre Right: ");
-    // Serial.print(detectedCentreRight());
-    // Serial.print(" Far Left: ");
-    // Serial.print(detectedFarLeft());
-    // Serial.print(" Far Right");
-    // Serial.println(detectedFarRight());
-    // UpdateTOFL0();
-    // UpdateTOFL1();
+    if ( BlueButtonState() && timer > 2500 && !pwmOn ) {
+        pwmOn = true;
+    }
 
-    // UpdateIMU();
-    // if (updateSensorsTimer > 50) {
-    //     float acc = GetOrientationPitch();
-    //     Serial.print("Pitch: ");
-    //     Serial.print(acc);
-    //     float acc2 = GetOrientationRoll();
-    //     Serial.print(" Roll: ");
-    //     Serial.print(acc2);
-    //     float acc3 = GetOrientationYaw();
-    //     Serial.print(" Yaw: ");
-    //     Serial.print(acc3);
+    if ( updateFSMTimer > 70 ) {
+        processFSM(&fsm);
+        updateFSMTimer = 0;
+    }
 
-    //     if (checkTargetHeading(fsm.targetHeading)) {
-    //         Serial.print("At target heading");
-    //     }
-    //     Serial.println();
-    //     updateSensorsTimer = 0;
-    // }
+    if ( updatePWMTimer > 20 && pwmOn ) {
+        PIDMotorControl(&leftMotor);
+        PIDMotorControl(&rightMotor);
+        updatePWMTimer = 0;
+    }
 
-    // if (updateFSM > 71) {
-    //     processFSM(&fsm);
-    //     updateFSM = 0;
-    // //     Serial.println("--------------------");
-    // //     // Serial.print("Current Heading: ");
-    // //     // Serial.print(GetOrientationYaw());
-    // //     // Serial.print(" Target Heading: ");
-    // //     // Serial.println(fsm.targetHeading);
-    // }
-
-    // if (updatePWM > 19) {
-    //     updatePWM = 0;
-    //     PIDMotorControl(&leftMotor);
-    //     PIDMotorControl(&rightMotor);
-
-    // }
-
-    // if ( updateSensorsTimer > 51 ) {
-    //     UpdateTOFL0();
-    //     UpdateTOFL1();
-    //     UpdateIMU();
-    //     updateSensorsTimer = 0;
-    // }
-    // if ( updateFSM > 31 ) {
-    //     // processFSM(&fsm);
-    //     updateFSM = 0;
-    //     Serial.print("L0TL: ");
-    //     Serial.print(GetL0TL());
-    //     Serial.print(" L0TR: ");
-    //     Serial.print(GetL0TR());
-    //     Serial.print(" L0BL: ");
-    //     Serial.print(GetL0BL());
-    //     Serial.print(" L0BR: ");
-    //     Serial.println(GetL0BR());
-    //     Serial.print("L1TL: ");
-    //     Serial.print(GetL1TL());
-    //     Serial.print(" L1TR: ");
-    //     Serial.print(GetL1TR());
-    //     Serial.print(" L1BL: ");
-    //     Serial.print(GetL1BL());
-    //     Serial.print(" L1BR: ");
-    //     Serial.println(GetL1BR());
-    // }
+    if ( updateSensorsTimer > 50 ) {
+        UpdateTOFL0();
+        UpdateTOFL1();
+        UpdateIMU();
+        updateSensorsTimer = 0;
+    }
 }
